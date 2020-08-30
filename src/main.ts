776184dd12d55cbd7f65b462eb0acbeadb8f35c0
Francis <francis.auyeung@gmail.com>
2020-08-29 19:42:37 -0700

    use core.info

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,22 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
+    core.info(`info: START...`)
     const token = core.getInput('github-token', {required: true})
+    core.info(`info: token = ${token}`)
     core.debug(`debug: token = ${token}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const bodyDoesNotContain = core.getInput('bodyDoesNotContain', {required: false})
+    core.info(`info: bodyDoesNotContain = ${bodyDoesNotContain}`)
 
     const context = github.context;
     const contextStr = JSON.stringify(context, null, 2)
+    core.info(`info: context = ${contextStr}`)
     core.debug(`debug: context = ${contextStr}`)
 
     core.setOutput('diff', new Date().toTimeString())
+    core.info(`info: END...`)
   } catch (error) {
+    core.info(`info: error: ${error.message}`)
     core.setFailed(error.message)
   }
 }
