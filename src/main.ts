6449041fedfc1de870d5965f6271015949de7ba5
Francis <francis.auyeung@gmail.com>
2020-08-29 18:57:47 -0700

    Morph to use @actions/github and core

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('github-token', {required: true})
+    core.debug(`debug: token = ${token}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const context = github.context;
+    const contextStr = JSON.stringify(context, null, 2)
+    core.debug(`debug: context = ${contextStr}`)
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('diff', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
